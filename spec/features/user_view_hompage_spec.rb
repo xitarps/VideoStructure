a62539419f18b97e2ee17dde3468713476792835
xita Rps <xitarps@gmail.com>
2020-09-22 16:49:35 -0300

    Fix offences from first system configuration(not from features)

--- spec/features/user_view_hompage_spec.rb
+++ spec/features/user_view_hompage_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-feature 'view homepage' do
-  context 'user' do
-    it 'successfully' do
+feature 'view homepage -' do
+  context 'visitor' do
+    it 'access successfully' do
       # Arrange
     
       # Act
       visit root_path
 
       # Assert
-      expect(page).to have_content('Video Structure')
+      expect(page).to have_content(I18n.t(:video_structure_main_title).capitalize)
     end
   end
 end
