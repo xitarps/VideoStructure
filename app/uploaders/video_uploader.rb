20e626366d4b9e50d87f863dff767d6fc74b1e21
xita Rps <xitarps@gmail.com>
2020-09-24 00:35:52 -0300

    Change upload dir to 'use' heroku to storage videos

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -9,12 +9,22 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
+  # 
+  # def cache_dir
+  #   "#{Rails.root}/tmp/uploads"
+  # end
+
+  #----custom
+
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    nil
   end
-  
+
   def cache_dir
-    "#{Rails.root}/tmp/uploads"
+    "#{Rails.root}/tmp/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
